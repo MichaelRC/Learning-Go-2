5f4f6f8eecfedb2212fd872341b9583e55c5e72d
Michael Campitiello <michaelrcampi@gmail.com>
2022-04-21 14:02:35 -0700

    fixed the folder to include main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+
+	var whatToSay string
+	var i int
+
+	whatToSay = "Goodbye, cruel world."
+	fmt.Println(whatToSay)
+
+	i = 42
+
+	fmt.Println("i is set to", i)
+
+	whatWasSaid, theOtherThingThatWasSaid := saySomething()
+
+	fmt.Println(whatWasSaid, theOtherThingThatWasSaid)
+}
+
+func saySomething() (string, string) {
+	return "something", "else"
+}
